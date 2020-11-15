a49a911d66a6215827c816ef01ffdd740218d115
Jose Melero Martinez <jmelero08@gmail.com>
2020-11-15 23:34:18 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     index_page = Nokogiri::HTML(open(index_url))
     students = []
-    card.css("div.roster-cards-container").each do |card|
+    index_page.css("div.roster-cards-container").each do |card|
     end   
   end
 
